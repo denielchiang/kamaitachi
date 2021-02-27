7ec2a3f790fd3c239fc31175b8f9a8ea69f56e1e
Deniel Chiang <deniel@jay-creative.com>
2021-02-27 14:27:56 +0800

    change database url to input

--- bin/build.sh
+++ bin/build.sh
@@ -3,7 +3,8 @@
 set -o errexit
 
 export SECRET_KEY_BASE=`mix phx.gen.secret`
-export DATABASE_URL="postgresql://kamaitachi-app:vsues16r3f5eozzt@kamaitachi-dev-do-user-8786788-0.b.db.ondigitalocean.com:25060/kamaitachi?sslmode=require"
+read -p "Enter Database URL: "  database_url
+export DATABASE_URL=$database_url
 
 # Initial setup
 mix deps.get --only prod
