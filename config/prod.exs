06d1532bf1553845dda071f124f2f8238736ed79
Deniel Chiang <deniel@jay-creative.com>
2021-03-01 11:59:03 +0800

    change database config to prod

--- config/prod.exs
+++ config/prod.exs
@@ -19,6 +19,7 @@ config :logger, level: :info
 config :kamaitachi, Kamaitachi.Repo,
   adapter: Ecto.Adapters.Postgres,
   maintenance_database: "defaultdb",
+  host: "private-kamaitachi-db-do-user-8786788-0.b.db.ondigitalocean.com",
   database: "kamaitachi",
   port: 25060,
   ssl: true,
